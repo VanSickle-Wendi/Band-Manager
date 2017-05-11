cef307ba4df63fb02f39c5b3b0680bc4160b93c1
VanSickle-Wendi <gwendells@gmail.com>
2017-05-10 22:04:52 -0600

    5/10/17 Testing GitHub

---
+++ Band-Manager/src/band/manager/BandManager.java
@@ -0,0 +1,22 @@
+Testing GitHub
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package band.manager;
+
+/**
+ *
+ * @author Wendi
+ */
+public class BandManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
