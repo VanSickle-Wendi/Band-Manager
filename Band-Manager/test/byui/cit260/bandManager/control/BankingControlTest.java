174166a541f93925d9b4987f6eb798dc0ad65d0e
Crazian92 <kenji.i.akazawa@gmail.com>
2017-05-26 19:31:48 -0700

    5/26/2017 Lesson 6 Individual Assignment Added calcNewVehicleCost tests

--- Band-Manager/test/byui/cit260/bandManager/control/BankingControlTest.java
+++ Band-Manager/test/byui/cit260/bandManager/control/BankingControlTest.java
@@ -100,5 +100,108 @@ public class BankingControlTest {
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
     }
-    
+
+    /**
+     * Test of calcNewVehicleCost method, of class BankingControl.
+     */
+    @Test
+    public void testCalcNewVehicleCost() {
+        System.out.println("CalcNewVehicleCost");
+        //Test Case 1
+        System.out.println("\tTestCase1");
+        
+        double originalVehicleCost = 5000;
+        double currentPerformancePoints = 1000;
+        double newVehiclePrice = 30000;
+        BankingControl instance = new BankingControl();
+        double expResult = 29850;
+        double result = instance.calcNewVehicleCost(originalVehicleCost, currentPerformancePoints, newVehiclePrice);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+        
+        //Test Case 2
+        System.out.println("\tTestCase2");
+        
+        originalVehicleCost = 10000;
+        currentPerformancePoints = 2000;
+        newVehiclePrice = 30000;
+        expResult = 29400;
+        result = instance.calcNewVehicleCost(originalVehicleCost, currentPerformancePoints, newVehiclePrice);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+        
+        //Test Case 3
+        System.out.println("\tTestCase3");
+        
+        originalVehicleCost = 5000;
+        currentPerformancePoints = 3500;
+        newVehiclePrice = 30000;
+        expResult = 29475;
+        result = instance.calcNewVehicleCost(originalVehicleCost, currentPerformancePoints, newVehiclePrice);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+        
+        //Test Case 4
+        System.out.println("\tTestCase4");
+        
+        originalVehicleCost = 5000;
+        currentPerformancePoints = -1;
+        newVehiclePrice = 100000;
+        expResult = 100000.15;
+        result = instance.calcNewVehicleCost(originalVehicleCost, currentPerformancePoints, newVehiclePrice);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+        
+        //Test Case 5
+        System.out.println("\tTestCase5");
+        
+        originalVehicleCost = 5000;
+        currentPerformancePoints = 10000;
+        newVehiclePrice = 100000;
+        expResult = 98500;
+        result = instance.calcNewVehicleCost(originalVehicleCost, currentPerformancePoints, newVehiclePrice);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+        
+        //Test Case 6
+        System.out.println("\tTestCase6");
+        
+        originalVehicleCost = 30000;
+        currentPerformancePoints = 20000;
+        newVehiclePrice = 50000;
+        expResult = 32000;
+        result = instance.calcNewVehicleCost(originalVehicleCost, currentPerformancePoints, newVehiclePrice);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+         //Test Case 7
+        System.out.println("\tTestCase7");
+        
+        originalVehicleCost = 5000;
+        currentPerformancePoints = 1;
+        newVehiclePrice = 30000;
+        expResult = 29999.85;
+        result = instance.calcNewVehicleCost(originalVehicleCost, currentPerformancePoints, newVehiclePrice);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        
+        
+         //Test Case 8
+        System.out.println("\tTestCase8");
+        
+        originalVehicleCost = 60000;
+        currentPerformancePoints = 500000;
+        newVehiclePrice = 100000;
+        expResult = -800000;
+        //Need to put a cap in the code, if result is negative. Give car for free.
+        result = instance.calcNewVehicleCost(originalVehicleCost, currentPerformancePoints, newVehiclePrice);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+  
+    }
 }
