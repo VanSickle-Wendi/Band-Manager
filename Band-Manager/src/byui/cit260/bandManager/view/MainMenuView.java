f985815e714c95d811972197e8a9c687c4357350
Crazian92 <kenji.i.akazawa@gmail.com>
2017-06-01 16:05:19 -0700

    6/1/2017 Working build, working on Main Menu View Bottom of page 41

--- Band-Manager/src/byui/cit260/bandManager/view/MainMenuView.java
+++ Band-Manager/src/byui/cit260/bandManager/view/MainMenuView.java
@@ -10,9 +10,46 @@ package byui.cit260.bandManager.view;
  * @author shaza
  */
 public class MainMenuView {
+    
+    private String menu;
+    
+    public MainMenuView() {
+        this.menu = "\n"
+                    + "\n-------------------------------------------------"
+                    + "\n| Main Menu |"
+                    + "\n--------------------------------------------------"
+                    + "\nN -- Start new game"
+                    + "\nL -- Load a saved game"
+                    + "\nH -- Help on how to play the game"
+                    + "\nS -- Save game"
+                    + "\nQ -- Quit and Exit Game"
+                    + "\n--------------------------------------------------";
+    }
+    
 
-    void displayMainMenuView() {
-        System.out.println("\ndisplayMainMenu");
+    public void displayMainMenuView() {
+        
+        boolean done = false; // set flag to not done
+        do {
+            // prompt for and get players name
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q")) // user wants to quit
+                return; //exit the game
+            
+            // do the requested action and display the next view
+            done = this.doAction(menuOption);
+            
+        } while (!done);
+    }
+
+    private String getMenuOption() {
+        System.out.println("\n*** getMenuOption() function called ***");
+        return "N";
+    }
+
+    private boolean doAction(String menuOption) {
+        System.out.println("\n*** doAction() function called ***");
+        return true;
     }
     
 }
