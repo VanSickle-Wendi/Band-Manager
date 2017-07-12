872200b4d75dc64386cad06d1b21f728d5f5dfa2
Polfer-Shannon <shannon.polfer@gmail.com>
2017-07-12 01:59:06 -0600

    7/11/2017 Finally finished the InstrumentReportView Class. Prints a report through Music Store, Purchase Equipment, Print List of Instruments.

--- Band-Manager/src/byui/cit260/bandManager/view/InstrumentReportView.java
+++ Band-Manager/src/byui/cit260/bandManager/view/InstrumentReportView.java
@@ -8,7 +8,12 @@ package byui.cit260.bandManager.view;
 import band.manager.BandManager;
 import byui.cit260.bandManager.control.EquipmentControl;
 import byui.cit260.bandManager.control.GameControl;
+import static byui.cit260.bandManager.control.GameControl.MusicStoreScene;
+import byui.cit260.bandManager.exceptions.EquipmentControlException;
+import byui.cit260.bandManager.model.Game;
 import byui.cit260.bandManager.model.Instrument;
+import byui.cit260.bandManager.model.MusicStoreScene;
+import java.io.PrintWriter;
 import java.text.DecimalFormat;
 
 /**
@@ -16,6 +21,9 @@ import java.text.DecimalFormat;
  * @author shaza
  */
 public class InstrumentReportView extends InteractiveView {
+
+    private final MusicStoreScene scene = new MusicStoreScene();
+    private Game game;
 
     public InstrumentReportView() {
 
@@ -27,6 +35,10 @@ public class InstrumentReportView extends InteractiveView {
                 + "\n                                                 "
                 + "\n-------------------------------------------------"
         );
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
     }
 
     @Override
@@ -61,21 +73,71 @@ public class InstrumentReportView extends InteractiveView {
 
         try {
             // save the report to the specified file TODO change to a map report
-            GameControl.mapReport(BandManager.getCurrentGame().getMap().getLocations(), value);
+            instrumentReport(scene.getDrums(), scene.getGuitar(), scene.getBass(), scene.getKeyboard(), scene.getMic(), value);
         } catch (Exception e) {
-            ErrorView.display("MapReportView", e.getMessage());
+            ErrorView.display("InstrumentReportView", e.getMessage());
         }
+
+        this.console.println("Success! Your report file was saved to: " + value);
 
         return false;
 
     }
-    
-   // public static void instrumentReport(Instrument[] purchasedInstruments, String filePath){
-      //  try (PrintWriter out = new PrintWriter(filePath)) {
-            
-         //   out.println("\n\n           Inventory Report                             ");
-        //    out.printf()
-       // }
-        
-   // }
+
+    public static void instrumentReport(Instrument[] drums, Instrument[] guitar, Instrument[] bass,
+            Instrument[] keyboard, Instrument[] mic, String filePath) {
+        PrintWriter out = null;
+
+        try {
+            out = new PrintWriter(filePath);
+
+            out.println("\n\n                           Instrument Report                         ");
+            out.printf("%n%-25s%-45s%-10s%n", "Type", "Description", "Price #");
+            out.printf("%n%-25s%-45s%-10s%n", "-----", "-----------------", "----------");
+            for (int i = 0; i < drums.length; i++) {
+                out.printf("%n%-25s%-45s%-10s%n", drums[i].getType(),
+                        drums[i].getDescription(),
+                        drums[i].getPrice());
+
+            }
+            for (int i = 0; i < guitar.length; i++) {
+                out.printf("%n%-25s%-45s%-10s%n", guitar[i].getType(),
+                        guitar[i].getDescription(),
+                        guitar[i].getPrice());
+
+            }
+            for (int i = 0; i < bass.length; i++) {
+                out.printf("%n%-25s%-45s%-10s%n", bass[i].getType(),
+                        bass[i].getDescription(),
+                        bass[i].getPrice());
+
+            }
+            for (int i = 0; i < keyboard.length; i++) {
+                out.printf("%n%-25s%-45s%-10s%n", keyboard[i].getType(),
+                        keyboard[i].getDescription(),
+                        keyboard[i].getPrice());
+
+            }
+            for (int i = 0; i < mic.length; i++) {
+                out.printf("%n%-25s%-45s%-10s%n", mic[i].getType(),
+                        mic[i].getDescription(),
+                        mic[i].getPrice());
+
+            }
+
+            out.flush();
+
+        } catch (Exception d) {
+            ErrorView.display("Error sending Instrument List to file", d.getMessage());
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (Exception e1) {
+
+                    ErrorView.display("Error closing file", e1.getMessage());
+                }
+            }
+        }
+    }
 }
